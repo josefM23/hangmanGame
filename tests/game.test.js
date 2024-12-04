4a01e37ae1d787681de23a2ca8ead56dfd35279b
Josef <jm224ae@student.lnu.se>
2024-12-04 21:47:25 +0100

    REFACTORING low level.

--- tests/game.test.js
+++ tests/game.test.js
@@ -40,27 +40,23 @@ describe('Game', () => {
     })
   })
 
-  describe('Correct and incorrect guesses', () => {
-    // Testing av rätta svaret.
+  describe('Validating correct and incorrect guesses', () => {
     test('should return true for a correct guess', () => {
       expect(game.isCorrectGuess('h')).toBe(true)
     })
 
-    // Testing av fell svaret.
     test('should return false for an incorrect guess', () => {
       expect(game.isCorrectGuess('x')).toBe(false)
     })
   })
 
-    // Testing av dublla bokstaver.(ignorera andra försåkt).
-  describe('Tracking guesses', () => {
+  describe('Handling and tracking duplicate and incorrect guesses', () => {
     test('should ignore duplicate guesses', () => {
       game.isCorrectGuess('h') // First guess.
       game.isCorrectGuess('h') // Duplicate guess.
       expect(game.guessedLetters).toEqual(['h']) // Only one 'h' in the list.
     })
 
-    // Testing av felaktiga gissningar.
     test('should track wrong guesses separately', () => {
       game.isCorrectGuess('x') // Incorrect guess.
       game.isCorrectGuess('y') // Another incorrect guess.
@@ -88,7 +84,7 @@ describe('Game', () => {
 
     test('should return true even with duplicate guesses', () => {
       game.isCorrectGuess('h')
-      game.isCorrectGuess('h') // Duplicate guess
+      game.isCorrectGuess('h') // Duplicate guess.
       game.isCorrectGuess('a')
       game.isCorrectGuess('n')
       game.isCorrectGuess('g')
